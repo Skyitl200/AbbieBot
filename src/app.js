b965c5eb5b76db34142ee58b718b92ec8bfa00ad
Skyitl200 <jessebusiness1995@gmail.com>
2026-09-19 23:49:11 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 ﻿import 'dotenv/config';
-import { Client, Collection, GatewayIntentBits } from 'discord.js';
+import { Client, Collection, GatewayIntentBits, Partials } from 'discord.js';
 import { REST } from '@discordjs/rest';
 import express from 'express';
 import cron from 'node-cron';
@@ -33,7 +33,13 @@ class TitanBot extends Client {
 
         GatewayIntentBits.GuildVoiceStates,             
 
-        GatewayIntentBits.GuildBans,                    
+                GatewayIntentBits.GuildBans,                    
+      ],
+
+      partials: [
+        Partials.Message,
+        Partials.Channel,
+        Partials.Reaction,
       ],
     });
 
