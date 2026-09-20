2bd4636015317251f9132c65d4e7f29a40d88e5b
Skyitl200 <jessebusiness1995@gmail.com>
2026-09-20 00:20:22 -0400

    Update messageReactionRemove.js

--- src/events/messageReactionRemove.js
+++ src/events/messageReactionRemove.js
@@ -4,8 +4,8 @@ import { CAREER_ROLES } from '../commands/Career_roles/careerroles.js';
 export default {
     name: Events.MessageReactionRemove,
 
-    async execute(reaction, user, client) {
-        try {
+async execute(reaction, user, details, client) {
+    try {
             // Ignore bots.
             if (user.bot) return;
 
